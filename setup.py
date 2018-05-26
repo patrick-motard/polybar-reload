4e6a7a6c1e41cd83e8e578eb7ba51b37f451be68
patrick-motard <motard19@gmail.com>
2018-05-26 18:46:07 -0500

    Added missing dependencies, and added standard files
     ## Added
    - now including an __init__.py, still need to add contents to it
    - .gitignore
    
     ## Fixed
    - project now specifies it's dependencies so that they're download as part of install

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
     name = 'polybar-reload',
     packages = ['polybar-reload'],
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'reloads polybar when screen resolution changes',
     author = 'Patrick Motard',
     author_email = 'motard19@gmail.com',
     url = 'https://github.com/patrick-motard/polybar-reload',
-    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.0.tar.gz',
+    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.1.tar.gz',
     keywords = ['polybar', 'reload', 'resize', 'resolution'],
-    classifiers = []
+    classifiers = [],
+    install_requires = [
+        'Xlib'
+    ]
 )
