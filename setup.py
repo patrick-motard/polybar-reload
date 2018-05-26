77d6122504be1bd7703ef310455f83d29c9a838d
patrick-motard <motard19@gmail.com>
2018-05-25 22:01:47 -0500

    added download_url to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Patrick Motard',
     author_email = 'motard19@gmail.com',
     url = 'https://github.com/patrick-motard/polybar-reload',
-    download_url = '',
+    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.0.tar.gz',
     keywords = ['polybar', 'reload', 'resize', 'resolution'],
     classifiers = []
 )
