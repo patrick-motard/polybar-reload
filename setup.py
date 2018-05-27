e3448f2249448f4ee89fb466370be2fc85e5b6ab
patrick-motard <motard19@gmail.com>
2018-05-26 20:50:35 -0500

    added missing binary in setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 setup(
     name = 'polybar-reload',
     packages = ['polybar-reload'],
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'reloads polybar when screen resolution changes',
     author = 'Patrick Motard',
     author_email = 'motard19@gmail.com',
     url = 'https://github.com/patrick-motard/polybar-reload',
-    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.2.tar.gz',
+    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.3.tar.gz',
     keywords = ['polybar', 'reload', 'resize', 'resolution'],
     classifiers = [
         'Programming Language :: Python :: 3'
     ],
     scripts = [
-        ''
+        'bin/polybar-reload'
     ],
     install_requires = [
         'Xlib'
