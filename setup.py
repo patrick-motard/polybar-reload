8b0ea28672beb6b7a067e6828395a66d7f967cb7
patrick-motard <motard19@gmail.com>
2018-05-26 20:38:20 -0500

    should now function as a script
     ## Changed
    
    - should now work as a script you can call in the terminal, and should be added
      to PATH on install
    
     ## Fixed
    
    - setup.py now correctly specifies this package is compatible only with python 3

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup
 setup(
     name = 'polybar-reload',
     packages = ['polybar-reload'],
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'reloads polybar when screen resolution changes',
     author = 'Patrick Motard',
     author_email = 'motard19@gmail.com',
     url = 'https://github.com/patrick-motard/polybar-reload',
-    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.1.tar.gz',
+    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.2.tar.gz',
     keywords = ['polybar', 'reload', 'resize', 'resolution'],
-    classifiers = [],
+    classifiers = [
+        'Programming Language :: Python :: 3'
+    ],
+    scripts = [
+        ''
+    ],
     install_requires = [
         'Xlib'
     ]
